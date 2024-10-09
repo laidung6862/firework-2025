669b29d8ce91ec08a28e9838789c655b09b81e8b
laidung6862 <laidung6862@gmail.com>
2024-10-09 10:30:16 +0700

    git commit --amend

--- task1_sub01.js
+++ task1_sub01.js
@@ -1,3 +1,10 @@
 // test git commit --amend
 //--amned để ghi đè them code chỉnh sửa vào commit trước đó
 //dùng trong 1 pullresquest 1 commit
+
+//git add .
+//git commit --amend
+// :wq
+//git push origin task1 -f
+
+//-f để ghi đè commit trước đó
